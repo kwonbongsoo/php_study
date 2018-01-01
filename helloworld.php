c4305f6d9b1d2f1ea12f9dbb8d77d7ab8c817a67
kwonbongsoo <star1231076@gmail.com>
2018-01-01 15:07:28 +0900

    study

---
+++ helloworld.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    echo "Hello world";<br>
+    <?php
+        echo "Hello world";
+    ?>
+</body>
+</html>
